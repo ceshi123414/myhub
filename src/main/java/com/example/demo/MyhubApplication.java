4ce0a13bccd5bb1b7b0cb3bba4857b67cc4eda4e
兜兜 <1042145895@qq.com>
2019-03-21 10:27:12 +0800

    测试时

--- src/main/java/com/example/demo/MyhubApplication.java
+++ src/main/java/com/example/demo/MyhubApplication.java
@@ -3,10 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages= {"com.accp*"})
+@SpringBootApplication(scanBasePackages= {"com.accp.*"})
 public class MyhubApplication {
 
 	public static void main(String[] args) {
+		System.out.println("杀杀杀111112222222111");
 		SpringApplication.run(MyhubApplication.class, args);
 	}
 
